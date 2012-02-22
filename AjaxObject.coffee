6b347d44ef35c392836edcbd5854ee82e504e0a5
Dustin Hiatt <dustin.hiatt@webfilings.com>
2012-02-22 10:13:10 -0600

    remove old event handlers as well as changing the code to dynamically spawn new threads as needed.  reuses all threads if possible to reduce script loading time.  first use of thread will be slowest.  speed still somewhat hindered by browser limitations.

--- AjaxObject.coffee
+++ AjaxObject.coffee
@@ -10,21 +10,45 @@ class @AjaxObject
   constructor: (path) ->
     if AjaxObject.hasWorker
       AjaxObject.callbacks = []
+      AjaxObject.workers = []
       if path?.length
         AjaxObject.path = path
-      AjaxObject.worker = new Worker(AjaxObject.path)
-      AjaxObject.worker.addEventListener 'message', (res) ->
-        fxns = AjaxObject._getCallback(res.data.id)
-        if !res.data.error
-          fxns.onSuccess res.data.response
-        else
-          fxns.onFailure res.data.response
 
+
+  @_spawnWorker = () ->
+    worker = new Worker(AjaxObject.path)
+    worker.addEventListener 'message', (res) ->
+      fxns = AjaxObject._getCallback(res.data.id)
+      if !res.data.error
+        fxns.onSuccess res.data.response
+      else
+        fxns.onFailure res.data.response
+      AjaxObject.workers.push @
+
+    AjaxObject.workers.push(worker)
+
+  @_getWorker = () ->
+    worker = AjaxObject.workers.pop()
+    if not worker
+      AjaxObject._spawnWorker()
+      return AjaxObject._getWorker()
+    return worker
+
+  @_removeWorker = (worker) ->
+    j = 0
+    for w in AjaxObject.workers
+      if worker is w
+        AjaxObject.workers.splice(j, 1)
+        return
+      j++
 
   @_getCallback = (index) ->
+    j = 0
     for i, fxns of AjaxObject.callbacks
       if fxns.id is index
+        AjaxObject.callbacks.splice j, 1
         return fxns.fxns
+      j++
     return null
 
   singleThread: (config, onSuccess, onFailure) ->
@@ -40,6 +64,8 @@ class @AjaxObject
       @singleThread config, onSuccess, onFailure
       return
 
+    worker = AjaxObject._getWorker()
+
     obj = new Object()
     obj.id = AjaxObject.callbackIndex
     AjaxObject.callbackIndex++
@@ -50,4 +76,4 @@ class @AjaxObject
     $.extend(true, tempConfig, config)
     tempConfig.id = obj.id
     AjaxObject.callbacks.push(obj)
-    AjaxObject.worker.postMessage tempConfig
+    worker.postMessage tempConfig
--- AjaxObject.js
+++ AjaxObject.js
@@ -11,26 +11,62 @@
     function AjaxObject(path) {
       if (AjaxObject.hasWorker) {
         AjaxObject.callbacks = [];
+        AjaxObject.workers = [];
         if (path != null ? path.length : void 0) AjaxObject.path = path;
-        AjaxObject.worker = new Worker(AjaxObject.path);
-        AjaxObject.worker.addEventListener('message', function(res) {
-          var fxns;
-          fxns = AjaxObject._getCallback(res.data.id);
-          if (!res.data.error) {
-            return fxns.onSuccess(res.data.response);
-          } else {
-            return fxns.onFailure(res.data.response);
-          }
-        });
       }
     }
 
+    AjaxObject._spawnWorker = function() {
+      var worker;
+      worker = new Worker(AjaxObject.path);
+      worker.addEventListener('message', function(res) {
+        var fxns;
+        fxns = AjaxObject._getCallback(res.data.id);
+        if (!res.data.error) {
+          fxns.onSuccess(res.data.response);
+        } else {
+          fxns.onFailure(res.data.response);
+        }
+        return AjaxObject.workers.push(this);
+      });
+      return AjaxObject.workers.push(worker);
+    };
+
+    AjaxObject._getWorker = function() {
+      var worker;
+      worker = AjaxObject.workers.pop();
+      if (!worker) {
+        AjaxObject._spawnWorker();
+        return AjaxObject._getWorker();
+      }
+      return worker;
+    };
+
+    AjaxObject._removeWorker = function(worker) {
+      var j, w, _i, _len, _ref;
+      j = 0;
+      _ref = AjaxObject.workers;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        w = _ref[_i];
+        if (worker === w) {
+          AjaxObject.workers.splice(j, 1);
+          return;
+        }
+        j++;
+      }
+    };
+
     AjaxObject._getCallback = function(index) {
-      var fxns, i, _ref;
+      var fxns, i, j, _ref;
+      j = 0;
       _ref = AjaxObject.callbacks;
       for (i in _ref) {
         fxns = _ref[i];
-        if (fxns.id === index) return fxns.fxns;
+        if (fxns.id === index) {
+          AjaxObject.callbacks.splice(j, 1);
+          return fxns.fxns;
+        }
+        j++;
       }
       return null;
     };
@@ -49,11 +85,12 @@
     };
 
     AjaxObject.prototype.multiThread = function(config, onSuccess, onFailure) {
-      var obj, tempConfig;
+      var obj, tempConfig, worker;
       if (!AjaxObject.hasWorker) {
         this.singleThread(config, onSuccess, onFailure);
         return;
       }
+      worker = AjaxObject._getWorker();
       obj = new Object();
       obj.id = AjaxObject.callbackIndex;
       AjaxObject.callbackIndex++;
@@ -64,7 +101,7 @@
       $.extend(true, tempConfig, config);
       tempConfig.id = obj.id;
       AjaxObject.callbacks.push(obj);
-      return AjaxObject.worker.postMessage(tempConfig);
+      return worker.postMessage(tempConfig);
     };
 
     return AjaxObject;
